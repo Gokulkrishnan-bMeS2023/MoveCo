dcc56c170ddfcc626e9cf3788c19424088dbf660
Gokul Krishnan <gokulkrishnan20092002@gmail.com>
2026-01-28 17:48:48 +0530

    add scrollview

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import { Suspense } from "react";
 import { UrlPages } from "./UrlPages";
 import NotFound from "../pages/NotFound";
+import ScrollToTopButton from "../animations/ScrollToTopButton";
 
 const App = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {UrlPages.map(({ path, component: Component }) => (
-          <Route key={path} path={path} element={<Component />} />
-        ))}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {UrlPages.map(({ path, component: Component }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+      <ScrollToTopButton />
+    </>
   );
 };
 
