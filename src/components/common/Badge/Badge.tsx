2caa2c99a4deab244555ef9cb579353a4e1e12e2
Lavanya <lavanyaj0725@gmail.com>
2026-02-21 10:54:04 +0530

    change

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -1,31 +1,3 @@
-// import { Box } from "@chakra-ui/react";
-
-// interface CommonButtonProps {
-//   label?: string;
-// }
-
-// const Button = ({ label }: CommonButtonProps) => {
-//   return (
-//     <Box
-//       px={6}
-//       py={2}
-//       fontWeight="400"
-//       transition="all 0.2s"
-//       fontSize="xl"
-//       rounded="full"
-//       // mb={{ base: 4, lg: 6 }}
-//       bg={"brand.primary"}
-//       w={"fit-content"}
-//       color={"brand.white"}
-//     >
-//       {label}
-//     </Box>
-//   );
-// };
-
-// export default Button;
-
-
 import { Box } from "@chakra-ui/react";
 
 interface CommonBadgeProps {
