6e1442c8b859ab26266b09590d240297342e0bb6
Gokul Krishnan <gokulkrishnan20092002@gmail.com>
2026-02-26 16:40:53 +0530

    change overall

--- src/api/testimonialService.ts
+++ src/api/testimonialService.ts
@@ -1,22 +1,3 @@
-// import { api } from "./api";
-
-// export interface TestimonialPayload {
-//   firstName: string;
-//   lastName: string;
-//   moveDate: string;
-//   email: string;
-//   comments: string;
-//   isActive: boolean;
-// }
-
-// export const postTestimonial = (data: TestimonialPayload) => {
-//   return api.post("/v1/customer-testimonials", data);
-// };
-
-// export const getTestimonial = () => {
-//   return api.get("/v1/customer-testimonials");
-// };
-
 import { api } from "./api";
 
 export interface TestimonialPayload {
